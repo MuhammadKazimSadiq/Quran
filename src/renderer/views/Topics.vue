c710321713cb3899b2033efd6071edadd95ddd05
Muhammad Kazim <md.kazimsadiq@gmail.com>
2022-07-31 00:03:01 +0430

    add topic accordion

--- src/renderer/views/Topics.vue
+++ src/renderer/views/Topics.vue
@@ -1,3 +1,43 @@
 <template>
   <h1 class="text-center text-3xl dark:text-white">موضوعات</h1>
+
+  <div class="mx-auto mt-8 w-full rounded-2xl bg-white p-4 dark:bg-gray-800">
+    <Disclosure v-slot="{ open }" v-for="topic in topics">
+      <DisclosureButton
+        class="my-2 flex w-full justify-between rounded-lg bg-gray-100 p-4 text-left text-sm font-medium text-gray-900 hover:bg-gray-200 focus:outline-none focus-visible:ring focus-visible:ring-gray-500 focus-visible:ring-opacity-75 dark:bg-gray-900 dark:text-white"
+      >
+        <span class="text-xl">{{ topic.name }}</span>
+        <ChevronUpIcon
+          :class="open ? '' : 'rotate-180 transform'"
+          class="h-5 w-5 text-gray-500"
+        />
+      </DisclosureButton>
+      <transition
+        enter-active-class="transition duration-100 ease-out"
+        enter-from-class="transform scale-95 opacity-0"
+        enter-to-class="transform scale-100 opacity-100"
+        leave-active-class="transition duration-75 ease-out"
+        leave-from-class="transform scale-100 opacity-100"
+        leave-to-class="transform scale-95 opacity-0"
+      >
+        <DisclosurePanel
+          class="px-4 pt-4 pb-2 text-sm text-gray-900 dark:text-white"
+        >
+          بسم الله الرحمن الرحیم
+        </DisclosurePanel>
+      </transition>
+    </Disclosure>
+  </div>
 </template>
+
+<script setup>
+// components
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
+import ChevronUpIcon from "../components/icons/ChevronUpIcon.vue";
+
+const topics = [
+  { name: "عصمت", id: 1 },
+  { name: "امامت", id: 2 },
+  { name: "تجرد روح", id: 3 },
+];
+</script>
--- todo.md
+++ todo.md
@@ -1,38 +1,17 @@
-- pinia state management structure + fetching data --> done
-- skeleton loaders? --> not needed
-- navigation guards
-
-# Data Structure
-
-- chapters: [all chapters]
-- verses: [all verses]
-- translations: [all translations]
-
-# Side Navigation
-
-- Home
-- Read
-- Search
-- Bookmarked
-- Topics
-- Vocab
-
-FIX:
+# FIX:
 
 - add fancy verse number brackets
-- on search --> pluck id & use original verses array
-- copying searchResult verse --> copies highlight span tag
-- verse icons as props functionality
 - search tabs overflow scroll
 - command palette functionality
 - scrollTo only works on onMounted
 - scrollTo with use virtual scroll
 - save theme in localstorage
+- settings (save to localstorage?)
 
-LONG TERM:
+# LONG TERM:
 
 - search rules (add to settings)
-- db migrations
 - icons + topics + vocab + translations should all be customizable in the settings
 - on highlight word --> add to vocab || search
+- topics section
 - audio
